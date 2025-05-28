b135562d7d1a0ef9a942bbccc89eadd629f72ba9
IvanBallesteros <ivan.ballesteros.felipe@estudiantat.upc.edu>
2025-05-28 20:13:51 +0200

    Tests running

--- src/test/java/fr/inria/astor/test/repair/approaches/JGenProgTest.java
+++ src/test/java/fr/inria/astor/test/repair/approaches/JGenProgTest.java
@@ -477,7 +477,7 @@ public class JGenProgTest extends BaseEvolutionaryTest {
 		cs.command.put("-parameters",
 			"llmService" + File.pathSeparator + "ollama" +  // LLM
 			File.pathSeparator + "llmmodel" + File.pathSeparator + "codellama:13b" +  // Use CodeLlama model
-			File.pathSeparator + "maxsuggestionsperpoint" + File.pathSeparator + "0" + // Only generate 1 suggestion per point
+			File.pathSeparator + "maxsuggestionsperpoint" + File.pathSeparator + "3" + // Only generate 1 suggestion per point
 			File.pathSeparator + "llmprompttemplate" + File.pathSeparator + "MULTIPLE_SOLUTIONS" + // Use the detailed repair template
 			File.pathSeparator + "stopfirst" + File.pathSeparator + "true" // Stop at first solution
 		);
